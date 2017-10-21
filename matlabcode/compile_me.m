cac2f9fb66c77d2bcd3290be4bba4edfa0e43cf2
gunnar <gunnarhorve@gmail.com>
2017-10-21 14:46:42 -0400

    first build

--- matlabcode/compile_me.m
+++ matlabcode/compile_me.m
@@ -2,6 +2,9 @@
 addpath(fullfile(pwd,'utils'));
 addpath(fullfile(pwd,'callbacks'));
 
+%desperation
+% javaaddpath(fullfile(matlabroot,'../../../../home/gunnar/MatlabWeBlab/matlabcode','MatlabServer.jar'));
+
 % responds to URL's /hi && /HelloWorlds
 basic_server=createMatlabServer(4444)
 set(basic_server,'serverinvokedcallback',@HTMLCallback)
@@ -9,16 +12,16 @@ basic_server.startServer()
 
 %magic stuff for compiling
 %mcc -m -C -d exe_webserver compile_me.m -a ./callbacks -a ./utils
-% global N      %total number of calls
-% global E      %total number of errors
-% global TCALL  %instant of last call
-% 
-% N=0;
-% E=0;
-% TCALL=now;
-% 
-% while 1
-%     disp(sprintf('%s:  #calls: %d   #errors: %d   Last call: %s',...
-%         datestr(now),N,E,datestr(TCALL)));
-%     pause(900) %wait 15 minutes
-% end
+global N      %total number of calls
+global E      %total number of errors
+global TCALL  %instant of last call
+
+N=0;
+E=0;
+TCALL=now;
+
+while 1
+    disp(sprintf('%s:  #calls: %d   #errors: %d   Last call: %s',...
+        datestr(now),N,E,datestr(TCALL)));
+    pause(900) %wait 15 minutes
+end
